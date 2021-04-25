3040289a358e7e4a84937c673e9c411846cda889
marcin.sieradzki <yaamu1122@gmail.com>
2021-04-25 17:17:35 +0200

    config tailwind

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
-import { createApp } from 'vue'
-import './tailwind.css'
-import App from './App.vue'
-import { routes } from './routes.js'
-import { createRouter, createWebHistory } from 'vue-router'
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import { createApp } from "vue";
+import { routes } from "./routes.js";
+import { createRouter, createWebHistory } from "vue-router";
+import "./tailwind.css";
+import "primevue/resources/themes/arya-green/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 
-const app = createApp(App)
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+
+const app = createApp(App);
+
+app.component("Button", Button);
+app.component("InputText", InputText);
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
-
-app.use(router)
-app.mount('#app')
+});
+app.use(PrimeVue);
+app.use(router);
+app.mount("#app");
